673106d198618953c56d1c8fdf4033271f42c97a
Katia <145513374+K2t7a@users.noreply.github.com>
2024-09-15 19:35:49 -0600

    Update readme.py

---
+++ readme.py
@@ -0,0 +1,39 @@
+#readme
+# Proyecto de Ejercicios de Python
+
+'''
+estudiante: Katia Marilin Santos Avelar
+(trabaje individualmente, espero  no tener problemas por ello )
+
+## Descripción
+Este repositorio contiene varios ejercicios realizados en Python para practicar el uso de diferentes librerías y widgets. Los ejercicios incluyen:
+1. Una interfaz para mostrar nombre y edad.
+2. Una ventana para ingresar una clave secreta.
+3. Una ventana para ingresar número de cédula y nombre.
+4. Una ventana para registrar datos de tres mascotas.
+5. Una ventana para ingresar diez datos personales.
+'''
+
+
+#ejercicio 6
+'''
+Una clínica debe recopilar de manera precisa y rápida los datos básicos de los nuevos pacientes, 
+como género, país y edad. Los métodos manuales actuales son lentos y propensos a errores. 
+
+Problema 
+1: Errores de datos: Los pacientes pueden cometer errores al proporcionar datos. 
+2. Datos inconsistentes: Las entradas hechas manualmente pueden tener formatos diferentes. 
+3. Lentitud: El proceso puede llevar mucho tiempo, especialmente con muchos pacientes.
+
+La solución propuesta es una interfaz gráfica para PyQt5: 
+
+1. Radiobox para Género: le permite elegir entre las opciones predeterminadas de género para evitar errores de entrada. 
+2. Cuadro de países combinados: proporciona una lista de países para evitar errores de escritura y garantizar una selección precisa. 
+3. La caja de edad de Spinbox permite ingresar una edad dentro de un rango determinado, lo que evita entradas incorrectas. 
+
+¿Por qué se seleccionó esta solución?
+Precisión: Los controles predefinidos evitan errores de entrada de datos.
+Eficiencia: facilita la entrada estandarizada rápida.
+Uniformidad: garantiza que los datos sean precisos y consistentes.
+
+'''
